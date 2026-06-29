41838ba791268b86d17d34241986097755cee338
Nicolas Pettican <nicolaspettican@gmail.com>
2026-06-29 15:02:53 +0530

    Add funded and sponsored links in footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -17,6 +17,13 @@ export default ((opts?: Options) => {
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © Tenpa Bhikshu's Wiki {year}
         </p>
+        <p>
+          This wiki was created with funding from{" "}
+          <a href="https://khyentsefoundation.org/">Khyentse Foundation</a> and is now sponsored by{" "}
+          <a href="https://theofferingbowl.com/">Offering Bowl</a>. If you would like to see it
+          maintained and expanded, please consider{" "}
+          <a href="https://theofferingbowl.com/">donating to Offering Bowl</a>.
+        </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
